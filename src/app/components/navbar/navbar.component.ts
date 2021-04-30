8b1412a39a82fb3bf85e613d139ba5d5e1d06416
sherifHelmy <helmysherif792@gmail.com>
2021-04-30 11:24:34 +0200

    movies

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -8,11 +8,13 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class NavbarComponent implements OnInit {
   isLoggedIn = false;
+  username = '';
   constructor(private _authServices:AuthService , private _Router:Router) {
     _authServices.currentUser.subscribe((res:any) => {
         if(res != null)
         {
           this.isLoggedIn = true;
+          this.username = JSON.parse(res).first_name + JSON.parse(res).last_name
         } else {
           this.isLoggedIn = false;
         }
